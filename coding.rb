8fcdd74a9b51b9ac74d4d4dc964589d829d64d10
r.hater <r.hater02@gmail.com>
2021-12-21 20:08:22 +0300

    save 1

---
+++ coding.rb
@@ -0,0 +1,23 @@
+ require 'digest'
+  
+  puts 'Введите слово или фразу для шифрования:'
+  string = STDIN.gets.chomp
+  
+  puts 'Каким способом зашифровать:'
+  puts '1. MD5'
+  puts '2. SHA1'
+  encrypt_method = STDIN.gets.to_i
+  
+  until encrypt_method.between?(1, 2)
+    puts 'Выберите 1 или 2'
+    encrypt_method = STDIN.gets.to_i
+  end
+  
+  puts 'Вот что получилось:'
+  
+  case encrypt_method
+  when 1
+    puts Digest::MD5.hexdigest(string)
+  when 2
+    puts Digest::SHA1.hexdigest(string)
+  end
